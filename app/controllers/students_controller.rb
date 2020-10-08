91915d513ec19062ad69f6a9451f5c33b65a4bc1
Jorge Ramirez <joramirez96@gmail.com>
2020-10-08 21:05:53 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,9 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
+    session[:form_params] = params.inspect
     @student.save
     redirect_to student_path(@student)
   end
